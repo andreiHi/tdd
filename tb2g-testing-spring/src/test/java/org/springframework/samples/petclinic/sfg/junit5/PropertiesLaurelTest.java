1d430304719b657c6e7b961c20f1537ec61f3e83
Andrei Hincu <a.hincu@octopod.ru>
2022-05-07 10:54:09 +0300

    add testPropertySource for laurel test resource

---
+++ tb2g-testing-spring/src/test/java/org/springframework/samples/petclinic/sfg/junit5/PropertiesLaurelTest.java
@@ -0,0 +1,33 @@
+package org.springframework.samples.petclinic.sfg.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.samples.petclinic.sfg.HearingInterpret;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestPropertySource("classpath:laurel.properties")
+@ActiveProfiles("laurel-properties")
+@SpringJUnitConfig(classes = {PropertiesLaurelTest.TestConfig.class})
+class PropertiesLaurelTest {
+
+    @Configuration
+    @ComponentScan("org.springframework.samples.petclinic.sfg")
+    static class TestConfig {
+
+    }
+    @Autowired
+    HearingInterpret hearingInterpret;
+
+    @Test
+    void whatIHeard() {
+        String heard = hearingInterpret.whatIHeard();
+        assertEquals("LaUREl", heard);
+    }
+}
